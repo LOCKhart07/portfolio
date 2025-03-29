a01afe32fb06208f75c58140a9ef5696507a34fc
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-03-29 16:20:58 +0530

    add environment specification for GitHub Pages and update console logging method for Dato CMS token

--- src/queries/getDatoCmsToken.ts
+++ src/queries/getDatoCmsToken.ts
@@ -1,6 +1,6 @@
 // getDatoCmsToken.ts
 
 export const getDatoCmsToken = (): string => {
-  console.log("BRUHHH", process.env.REACT_APP_DATOCMSTOKEN_DEFAULT)
+  window.console.log("BRUHHH", process.env.REACT_APP_DATOCMSTOKEN_DEFAULT)
   return process.env.REACT_APP_DATOCMSTOKEN_DEFAULT  ?? '';
 };
