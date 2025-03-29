404e425021286b2e4ab01178ec414774e5736929
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-03-29 16:24:17 +0530

    remove debug log for Dato CMS token retrieval

--- src/queries/getDatoCmsToken.ts
+++ src/queries/getDatoCmsToken.ts
@@ -1,6 +1,5 @@
 // getDatoCmsToken.ts
 
 export const getDatoCmsToken = (): string => {
-  window.console.log("BRUHHH", process.env.REACT_APP_DATOCMSTOKEN_DEFAULT)
   return process.env.REACT_APP_DATOCMSTOKEN_DEFAULT  ?? '';
 };
