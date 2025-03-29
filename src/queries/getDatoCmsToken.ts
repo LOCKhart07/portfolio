20e9518cc36f7353be42e4f0be7a59ac940bcdfe
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-03-29 16:14:01 +0530

    add debug log for Dato CMS token retrieval

--- src/queries/getDatoCmsToken.ts
+++ src/queries/getDatoCmsToken.ts
@@ -1,5 +1,6 @@
 // getDatoCmsToken.ts
 
 export const getDatoCmsToken = (): string => {
+  console.log("BRUHHH", process.env.REACT_APP_DATOCMSTOKEN_DEFAULT)
   return process.env.REACT_APP_DATOCMSTOKEN_DEFAULT  ?? '';
 };
