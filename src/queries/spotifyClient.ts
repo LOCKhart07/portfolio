e4c2e476b6cb1bde1235029d698f0ae38895927a
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-04-02 02:18:02 +0530

    Update Spotify API base URL to use environment variable for better configuration management

--- src/queries/spotifyClient.ts
+++ src/queries/spotifyClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getSpotifyToken } from './getSpotifyToken';
 
 
-const SPOTIFY_STATS_BASE_URL = 'https://lockhart.in/spotify-stats/api';
+const SPOTIFY_STATS_BASE_URL = process.env.REACT_APP_SPOTIFY_STATS_API_BASE_URL ?? '';
 
 // const SPOTIFY_STATS_BASE_URL = 'http://localhost:9000/spotify-stats/api';
 const SPOTIFY_STATS_API_KEY = getSpotifyToken();
